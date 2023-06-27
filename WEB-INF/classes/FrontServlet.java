7f3457568ca3140757e7cd48b6b8966b25a99448
HaingoAdri <120141604+HaingoAdri@users.noreply.github.com>
2023-06-28 01:59:37 +1400

    Add files via upload

--- WEB-INF/classes/FrontServlet.java
+++ WEB-INF/classes/FrontServlet.java
@@ -1,13 +1,16 @@
 package etu2069.framework.servlet;
 
 import jakarta.servlet.http.*;
-
+import etu2069.framework.Mapping;
 import java.io.IOException;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 
 import jakarta.servlet.*;
 
 public class FrontServlet extends HttpServlet {
+
+    HashMap<String, Mapping> mappingUrls = new HashMap<String, Mapping>();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
@@ -25,5 +28,11 @@ public class FrontServlet extends HttpServlet {
        RequestDispatcher dispacth = request.getRequestDispatcher("Index.jsp");
 
        dispacth.forward(request,response);
+    }
+    public HashMap<String, Mapping> getMappingUrls() {
+        return mappingUrls;
+    }
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        this.mappingUrls = mappingUrls;
     }
 }
